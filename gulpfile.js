26946285b080e777a9f2fdc91ab61b71fd8be4d7
Tim Popham <popham@uw.edu>
2014-08-20 00:06:57 -0700

    Package management for build results

--- gulpfile.js
+++ gulpfile.js
@@ -14,10 +14,28 @@ var pretty = {
 var optimal = {};
 var uglify = function () { return uglify_(pretty); };
 
-gulp.task('build', ['amd', 'cjs']);
+gulp.task('build', ['amd', 'cjs', 'manage']);
+
+gulp.task('clean', function () {
+    return gulp.src(['amd', 'cjs'], { read : false })
+        .pipe(clean());
+});
+
+gulp.task('manage', ['bower', 'npm']);
+
+gulp.task('bower', function () {
+    return gulp.src('manage/bower.json')
+        .pipe(gulp.dest('amd'));
+});
+
+gulp.task('npm', function () {
+    return gulp.src('manage/package.json')
+        .pipe(gulp.dest('cjs'));
+});
 
 gulp.task('amd', function () {
     return gulp.src('lib/**/*.js')
+        .pipe(uglify())
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(gulp.dest('amd'));
@@ -25,6 +43,7 @@ gulp.task('amd', function () {
 
 gulp.task('cjs', function () {
     return gulp.src('lib/**/*.js')
+        .pipe(uglify())
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(nodefy())
