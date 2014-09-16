65bd2fe374fa30841c9853d2c229203ff4a0e8cc
Tim Popham <popham@uw.edu>
2014-09-15 19:06:01 -0700

    Initial

---
+++ builder/Allocator.js
@@ -0,0 +1,18 @@
+var Arena = require('./Arena');
+    var Allocator = function() {};
+    Allocator.allocate = function(bytes) {
+        var segment = new Uint8Array(bytes);
+        return segment;
+    };
+    Allocator.prototype.createArena = function(size) {
+        return new Builder(Allocator.allocate, size);
+    };
+    Allocator.prototype.initRoot = function(Struct) {
+        var arena = this.createArena();
+        return arena.initRoot(Struct);
+    };
+    Allocator.prototype.setRoot = function(reader) {
+        var arena = this.createArena();
+        return arena.setRoot(reader);
+    };
+    module.exports = Allocator;
---
+++ builder/AnyPointer.js
@@ -0,0 +1,18 @@
+var Reader = require('../reader/AnyPointer');
+    var Any = function(arena, pointer, depth) {
+        this._arena = arena;
+        this._pointer = pointer;
+        this._depth = depth;
+    };
+    Any.prototype._bytes = Reader.prototype._bytes;
+    Any.prototype.cast = function(Derefable) {
+        /*
+         * No increment on `depth` since the caller of `deref` has already
+         * incremented.
+         */
+        return Derefable.deref(this._arena, this._pointer, this._depth);
+    };
+    Any.deref = function(arena, pointer, depth) {
+        return new Any(arena, pointer, depth);
+    };
+    module.exports = Any;
