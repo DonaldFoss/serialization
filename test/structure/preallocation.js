f9e0f27bcf3adea05105c03c9c3b721f40f8ccbe
Tim Popham <popham@uw.edu>
2014-10-25 10:18:28 -0700

    Add intersegment testing

--- test/structure/preallocation.js
+++ test/structure/preallocation.js
@@ -43,3 +43,45 @@ describe ('Preallocated intrasegment struct pointer', function () {
         assert.equal(blob.position + ct.dataBytes + ct.pointersBytes, layout.end);
     });
 });
+
+describe ('Preallocated intersegment struct pointer', function () {
+    var segment0=new Buffer(80); segment0._id=0; segment0._position=80;
+    var segment1=new Buffer(80); segment1._id=1; segment1._position=80;
+    segment0.fill(0);
+    segment1.fill(0);
+    var p = { segment:segment0, position:8 };
+    var land = { segment:segment1, position:24 };
+    var blob = { segment:segment1, position:32 };
+    builder.preallocated(p, blob, ct);
+
+    it ('should get populated according to specification', function () {
+        assert.equal(segment0[p.position], 2 | (0<<2) | (offset<<3));
+        assert.equal(segment0[p.position+1], 0);
+        assert.equal(segment0[p.position+2], 0);
+        assert.equal(segment0[p.position+3], 0);
+        assert.equal(segment0[p.position+4], 1);
+        assert.equal(segment0[p.position+5], 0);
+        assert.equal(segment0[p.position+6], 0);
+        assert.equal(segment0[p.position+7], 0);
+    });
+
+    it ('should point to a landing pad that is immediately followed by its blob', function () {
+        assert.equal(segment1[land.position], 0 | (0<<2));
+        assert.equal(segment1[land.position+1], 0);
+        assert.equal(segment1[land.position+2], 0);
+        assert.equal(segment1[land.position+3], 0);
+        assert.equal(segment1[land.position+4], ct.dataBytes>>>3);
+        assert.equal(segment1[land.position+5], 0);
+        assert.equal(segment1[land.position+6], ct.pointersBytes>>>3);
+        assert.equal(segment1[land.position+7], 0);
+    });
+
+    it ('should parametrize the layout of a blob', function () {
+        var layout = reader.intersegment(land, blob);
+        assert.equal(ct.meta, layout.meta);
+        assert.equal(segment1, layout.segment);
+        assert.equal(blob.position, layout.dataSection);
+        assert.equal(blob.position+ct.dataBytes, layout.pointersSection);
+        assert.equal(blob.position+ct.dataBytes+ct.pointersBytes, layout.end);
+    });
+});
